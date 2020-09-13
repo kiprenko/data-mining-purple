d380932fb30328cc33a650f4b55b94d4cf5b3c1b
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-09-14 00:39:32 +0300

    lab-1 - removed pie chart and created bar chart with words length

--- lab-work-1/word_len_plot.py
+++ lab-work-1/word_len_plot.py
@@ -1,15 +1,18 @@
 from matplotlib import pyplot as plt
 import csv
+import numpy as np
 
 
 SPAM_FILE = 'output/spam_words.csv'
 HAM_FILE = 'output/ham_words.csv'
 WORD = 'word'
 COUNT = 'count'
+SPAM_TITLE = 'Count of spam words by length chart'
+HAM_TITLE = 'Count of ham words by length chart'
 
 
-def main():
-    with open(SPAM_FILE) as f:
+def main(file_name, chart_title):
+    with open(file_name) as f:
         csv_reader = csv.DictReader(f)
         w_len_dict = {}
         w_count = 0
@@ -23,14 +26,17 @@ def main():
                 w_len_dict[w_len] = count_
 
     plt.style.use('fivethirtyeight')
-    plt.title('word length chart')
-    plt.tight_layout()
-    slices = w_len_dict.values()
-    labels = w_len_dict.keys()
-    plt.pie(slices, labels=labels)
+    plt.title(chart_title)
+    words = w_len_dict.keys()
+    x_indexes = np.arange(len(words))
+    plt.xticks(x_indexes, words)
+    plt.bar(x_indexes, w_len_dict.values(), color='#006a71', label='count of words by length')
+    plt.xlabel("word length")
+    plt.ylabel("count of words")
     plt.show()
 
 
 # Вивести на графіках розподіл по довжині слів для кожної категорії і середню довжину слів.
 if __name__ == '__main__':
-    main()
+    main(SPAM_FILE, SPAM_TITLE)
+    main(HAM_FILE, HAM_TITLE)
