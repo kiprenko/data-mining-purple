c79b29cc05d1265423c257f2c3068d8dc9d9badb
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-09-29 22:47:26 +0300

    lab-2 - developing a lab-2 workpiece

--- lab-2/TrainingDataProcessor.py
+++ lab-2/TrainingDataProcessor.py
@@ -1,3 +1,10 @@
 class TrainingDataProcessor:
-    def read_data(self):
-        print('hello world')
+    spam_file_name = ''
+    ham_file_name = ''
+
+    def __init__(self, spam_file_name, ham_file_name):
+        self.spam_file_name = spam_file_name
+        self.ham_file_name = ham_file_name
+
+    def print_file_names(self):
+        print(f"Spam file name '{self.spam_file_name}'\nHam file name '{self.ham_file_name}'")
--- lab-2/main.py
+++ lab-2/main.py
@@ -1,5 +1,5 @@
 from TrainingDataProcessor import TrainingDataProcessor
 
 if __name__ == '__main__':
-    data_processor = TrainingDataProcessor()
-    data_processor.read_data()
+    data_processor = TrainingDataProcessor('spam_words_count.csv', 'ham_words_count.csv')
+    data_processor.print_file_names()
