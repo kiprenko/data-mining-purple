00d5ab4f669462747af65902f8499fec36f8bb35
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-09-29 23:05:37 +0300

    lab-2 - developing a lab-2 workpiece

--- lab-2/TrainingDataProcessor.py
+++ lab-2/TrainingDataProcessor.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class TrainingDataProcessor:
     spam_file_name = ''
     ham_file_name = ''
@@ -8,3 +11,18 @@ class TrainingDataProcessor:
 
     def print_file_names(self):
         print(f"Spam file name '{self.spam_file_name}'\nHam file name '{self.ham_file_name}'")
+
+    def process_files(self):
+        self.__process_file(self.spam_file_name)
+        self.__process_file(self.ham_file_name)
+
+    def __process_file(self, file_name):
+        total_words_count = 0
+        words_popularity = {}
+        with open(file_name) as f:
+            csv_reader = csv.DictReader(f)
+            for row in csv_reader:
+                w_count = row['count']
+                total_words_count += int(w_count)
+                words_popularity[row['word']] = int(w_count) / total_words_count
+        print(words_popularity)
--- lab-2/main.py
+++ lab-2/main.py
@@ -3,3 +3,4 @@ from TrainingDataProcessor import TrainingDataProcessor
 if __name__ == '__main__':
     data_processor = TrainingDataProcessor('spam_words_count.csv', 'ham_words_count.csv')
     data_processor.print_file_names()
+    data_processor.process_files()
