7520bef7758d6b57acf08e255d9fb8cd7091b8af
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-11-11 12:27:21 +0200

    lab-1 - added normalization for the first lab

--- lab-1/msg_len_plot.py
+++ lab-1/msg_len_plot.py
@@ -13,9 +13,9 @@ MSG_KEY = 'v2'
 def calculate_avg(d):
     x = 0
     y = 0
-    for v in d:
-        c = v[1]
-        x += v[0] * c
+    for item in d.items():
+        c = item[1]
+        x += item[0] * c
         y += c
     return x / y
 
@@ -31,24 +31,27 @@ def main():
                 spam_w_list.append(msg_len)
             else:
                 ham_w_list.append(msg_len)
-        spam_w_list = Counter(spam_w_list).most_common()
-        ham_w_list = Counter(ham_w_list).most_common()
+        spam_w_dict = {word[0]: word[1] for word in Counter(spam_w_list).most_common()}
+        ham_w_dict = {word[0]: word[1] for word in Counter(ham_w_list).most_common()}
+        spam_w_count = sum(spam_w_dict.values())
+        ham_w_count = sum(ham_w_dict.values())
+        spam_w_dict = {word[0]: word[1] / spam_w_count for word in spam_w_dict.items()}
+        ham_w_dict = {word[0]: word[1] / ham_w_count for word in ham_w_dict.items()}
+        draw_chart(spam_w_dict, SPAM)
+        draw_chart(ham_w_dict, HAM)
 
-        draw_chart(spam_w_list, SPAM)
-        draw_chart(ham_w_list, HAM)
 
-
-def draw_chart(cnter, msg_type):
+def draw_chart(dict, msg_type):
     plt.style.use('fivethirtyeight')
     plt.title('{} message length chart'.format(msg_type))
-    count_of_words = [c[0] for c in cnter]
+    count_of_words = [c for c in dict.keys()]
     x_indexes = np.arange(len(count_of_words))
     plt.xticks(x_indexes, count_of_words)
-    plt.bar(x_indexes, [c[1] for c in cnter], color='#006a71', label='count of msg by length')
+    plt.bar(x_indexes, [c for c in dict.values()], color='#006a71', label='count of msg by length')
     plt.xlabel("message length")
     plt.ylabel("count of messages")
     plt.show()
-    print('Average {} message count = {}'.format(msg_type, str(calculate_avg(cnter))))
+    print('Average {} message count = {}'.format(msg_type, str(calculate_avg(dict))))
 
 
 # Вивести на графіках розподіл по довжині повідомлень для кожної категорії і середню довжину повідомлення.
