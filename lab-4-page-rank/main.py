90620fd4efead0ce7a04aef88af382c5908aa1b7
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-11-19 03:35:22 +0200

    lab-4 - started development of fourth lab.

--- lab-4-page-rank/main.py
+++ lab-4-page-rank/main.py
@@ -1,5 +1,27 @@
+import urllib.request as req
+
+import validators
+from bs4 import BeautifulSoup
+
+URL_TO_PARSE = 'http://thedemosite.co.uk/'
+VISITED_PAGES = []
+
+
 def main():
-    print('Hello, world!')
+    parse_site(req, 'http://thedemosite.co.uk/')
+
+
+def parse_site(_req, url):
+    VISITED_PAGES.append(url)
+    req_res = _req.urlopen(url)
+    soup = BeautifulSoup(req_res.read(), 'lxml')
+    for tag in soup.find_all('a'):
+        href_ = tag.attrs["href"]
+        if not href_.startswith('http') and not href_.startswith('javascript'):
+            print(href_)
+            url_to_page = URL_TO_PARSE + href_
+            if url_to_page not in VISITED_PAGES and validators.url(url_to_page):
+                parse_site(_req, url_to_page)
 
 
 if __name__ == '__main__':
