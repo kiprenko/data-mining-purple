27bc3a19b48fe05813f1d530984b2cc5206bda23
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-11-19 23:48:28 +0200

    lab-4 - added page rank calculation.

--- lab-4-page-rank/main.py
+++ lab-4-page-rank/main.py
@@ -1,5 +1,5 @@
+import json
 import urllib.request as req
-from pprint import pprint
 
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -9,46 +9,48 @@ from bs4 import BeautifulSoup
 HOST = 'http://thedemosite.co.uk/'
 PLOT_STYLE = 'fivethirtyeight'
 VISITED_PAGES = []
-PAGES_GRAPH = {}
+PAGE_DATA = {}
 
 
 def main():
     print(f'Started parsing of host {HOST}')
     collect_graph_data()
-    draw_graph()
+    page_rank = {k: v for k, v in sorted(nx.pagerank(draw_graph()).items(), key=lambda item: item[1], reverse=True)}
+    print('Page rank' + json.dumps(page_rank, indent=4))
 
 
 def collect_graph_data():
     parse_site()
-    pprint(PAGES_GRAPH)
+    print(json.dumps(PAGE_DATA, indent=4))
 
 
 def parse_site(url='/'):
     VISITED_PAGES.append(url)
-    PAGES_GRAPH[url] = []
+    PAGE_DATA[url] = []
     req_res = req.urlopen(HOST + url)
     soup = BeautifulSoup(req_res.read(), 'lxml')
     links = soup.find_all('a')
     for link in links:
         url_to_another_page = link.attrs['href']
         if not url_to_another_page.startswith('http') and not url_to_another_page.startswith('javascript'):
-            PAGES_GRAPH[url].append(url_to_another_page)
+            PAGE_DATA[url].append(url_to_another_page)
             if url_to_another_page not in VISITED_PAGES and validators.url(HOST + url_to_another_page):
                 parse_site(url_to_another_page)
 
 
 def draw_graph():
-    page_graph = nx.MultiDiGraph(pairs_list())
+    page_graph = nx.DiGraph(pairs_list())
     pos = nx.spring_layout(page_graph)
     nx.draw_networkx(page_graph, pos)
     plt.title(f'Graph of pages at {HOST}')
     plt.style.use(PLOT_STYLE)
     plt.show()
+    return page_graph
 
 
 def pairs_list():
     list_ = []
-    for page in PAGES_GRAPH.items():
+    for page in PAGE_DATA.items():
         for target_page in page[1]:
             list_.append((page[0], target_page))
     return list_
