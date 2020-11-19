4f7c4d69e9f952c7047f84b2a6d709b3f1bc3ead
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-11-19 20:01:09 +0200

    lab-4 - refactoring.

--- lab-4-page-rank/main.py
+++ lab-4-page-rank/main.py
@@ -4,28 +4,28 @@ from pprint import pprint
 import validators
 from bs4 import BeautifulSoup
 
-URL_TO_PARSE = 'http://thedemosite.co.uk/'
+HOST = 'http://thedemosite.co.uk/'
 VISITED_PAGES = []
 PAGES_GRAPH = {}
 
 
 def main():
-    parse_site(req, 'http://thedemosite.co.uk/')
+    print(f'Started parsing of host {HOST}')
+    parse_site()
 
 
-def parse_site(_req, url):
+def parse_site(url='/'):
     VISITED_PAGES.append(url)
     PAGES_GRAPH[url] = []
-    req_res = _req.urlopen(url)
+    req_res = req.urlopen(HOST + url)
     soup = BeautifulSoup(req_res.read(), 'lxml')
     links = soup.find_all('a')
     for link in links:
-        href_ = link.attrs['href']
-        if not href_.startswith('http') and not href_.startswith('javascript'):
-            url_to_page = URL_TO_PARSE + href_
-            PAGES_GRAPH[url].append(url_to_page)
-            if url_to_page not in VISITED_PAGES and validators.url(url_to_page):
-                parse_site(_req, url_to_page)
+        url_to_another_page = link.attrs['href']
+        if not url_to_another_page.startswith('http') and not url_to_another_page.startswith('javascript'):
+            PAGES_GRAPH[url].append(url_to_another_page)
+            if url_to_another_page not in VISITED_PAGES and validators.url(HOST + url_to_another_page):
+                parse_site(url_to_another_page)
 
 
 if __name__ == '__main__':
