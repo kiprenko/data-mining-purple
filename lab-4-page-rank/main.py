f67a06c897912a765a33bceab78fa0de12c36e3f
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-11-19 19:26:31 +0200

    lab-4 - implemented site parsing to collect page graph.

--- lab-4-page-rank/main.py
+++ lab-4-page-rank/main.py
@@ -1,10 +1,12 @@
 import urllib.request as req
+from pprint import pprint
 
 import validators
 from bs4 import BeautifulSoup
 
 URL_TO_PARSE = 'http://thedemosite.co.uk/'
 VISITED_PAGES = []
+PAGES_GRAPH = {}
 
 
 def main():
@@ -13,16 +15,19 @@ def main():
 
 def parse_site(_req, url):
     VISITED_PAGES.append(url)
+    PAGES_GRAPH[url] = []
     req_res = _req.urlopen(url)
     soup = BeautifulSoup(req_res.read(), 'lxml')
-    for tag in soup.find_all('a'):
-        href_ = tag.attrs["href"]
+    links = soup.find_all('a')
+    for link in links:
+        href_ = link.attrs['href']
         if not href_.startswith('http') and not href_.startswith('javascript'):
-            print(href_)
             url_to_page = URL_TO_PARSE + href_
+            PAGES_GRAPH[url].append(url_to_page)
             if url_to_page not in VISITED_PAGES and validators.url(url_to_page):
                 parse_site(_req, url_to_page)
 
 
 if __name__ == '__main__':
     main()
+    pprint(PAGES_GRAPH)
