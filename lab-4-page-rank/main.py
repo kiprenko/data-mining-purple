14e4eea436ab3f90c88140cc37559c91ad615f8f
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-11-19 22:44:56 +0200

    lab-4 - added page graph visualization.

--- lab-4-page-rank/main.py
+++ lab-4-page-rank/main.py
@@ -1,17 +1,26 @@
 import urllib.request as req
 from pprint import pprint
 
+import matplotlib.pyplot as plt
+import networkx as nx
 import validators
 from bs4 import BeautifulSoup
 
 HOST = 'http://thedemosite.co.uk/'
+PLOT_STYLE = 'fivethirtyeight'
 VISITED_PAGES = []
 PAGES_GRAPH = {}
 
 
 def main():
     print(f'Started parsing of host {HOST}')
+    collect_graph_data()
+    draw_graph()
+
+
+def collect_graph_data():
     parse_site()
+    pprint(PAGES_GRAPH)
 
 
 def parse_site(url='/'):
@@ -28,6 +37,22 @@ def parse_site(url='/'):
                 parse_site(url_to_another_page)
 
 
+def draw_graph():
+    page_graph = nx.MultiDiGraph(pairs_list())
+    pos = nx.spring_layout(page_graph)
+    nx.draw_networkx(page_graph, pos)
+    plt.title(f'Graph of pages at {HOST}')
+    plt.style.use(PLOT_STYLE)
+    plt.show()
+
+
+def pairs_list():
+    list_ = []
+    for page in PAGES_GRAPH.items():
+        for target_page in page[1]:
+            list_.append((page[0], target_page))
+    return list_
+
+
 if __name__ == '__main__':
     main()
-    pprint(PAGES_GRAPH)
